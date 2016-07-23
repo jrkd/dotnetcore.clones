63383f9dc4a233c2a7666b7be9a36084443b63f2
pull-jono <jono@pulldigital.com>
2016-07-23 22:44:31 +0100

    Removed scaffolding HTML. Now controllers and views are being rendered.

---
+++ ovalspace/Controllers/SpaceController.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using oval.models;
+
+namespace oval.controllers{
+    public class SpaceController : Controller
+    {
+        private OvalContext _context;
+        public SpaceController(OvalContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(){
+            return View("SpaceList");
+        }
+        public IActionResult List(){
+            return View("SpaceList");
+        }
+
+        public IActionResult Detail(int id){
+            return View("SpaceDetail", _context.Spaces.FirstOrDefault(space => space.ID == id));
+        }
+    }
+}
--- ovalspace/Models/OvalContext.cs
+++ ovalspace/Models/OvalContext.cs
@@ -5,9 +5,11 @@ namespace oval.models
 {
     public class OvalContext : DbContext
     {
+        
         public OvalContext(DbContextOptions<OvalContext> options)
             : base(options)
         { }
+        
         public DbSet<Space> Spaces { get; set; }
         public DbSet<Event> Events { get; set; }
     }
